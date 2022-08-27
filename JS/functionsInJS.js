d096d7f72a1f5bbb5328cfbed6da22fccc869414
karunasagar555 <skarunasagar3@gmail.com>
2022-08-28 00:10:30 +0530

    :fire: CallBack Functions, Async TASKS using CallBack Functions

--- JS/functionsInJS.js
+++ JS/functionsInJS.js
@@ -24,21 +24,48 @@
 
 // Named Function Expressions - we don't use an Anonymous function here instead we use function with a name
 
-var b = function xyz() {
-    console.log("called");
+// var b = function xyz() {
+//     console.log("called");
+// }
+// b();
+
+
+
+// // Difference between Function Parameters & Function Arguments
+
+// var b = function xyz(Param1, Param2) {
+//     console.log(" b called");
+// }
+// b(Arg1,Arg2);
+
+// // First Class Functions (First Class Citizens) - the ability to use functions as values (assigning to a variable) is what is called First class Functions (the aility to -> functions can be passed as arguments inside another function, functions can be returned from another function)
+
+// // callBack Functions 
+
+// function x(){
+
+// }
+// x(function y(){   // here function 'y' is known as callBack Function -> this function 'y' is called back sometime later 
+//                   // in the program that is why it is known as callBack Function.
+// })
+
+
+
+//  Async TASKS using CallBack Functions 
+
+setTimeout(function (){   //-> the setTimeout function here is taking a callBack Function 
+    console.log("Timer"); // -> the first parameter inside setTimeout function is a callBack Function which is,
+}, 5000);                 // -> passed into setTimeout function which is called back sometime later in the code,
+                          // -> after certain amount of time which we pass as 2nd arg.
+
+function x(y){
+    console.log("x");
+    y();
 }
-b();
+x(function(y){
+    console.log("y");
+});
 
-
-
-// Difference between Function Parameters & Function Arguments
-
-var b = function xyz(Param1, Param2) {
-    console.log(" b called");
-}
-b(Arg1,Arg2);
-
-// First Class Functions - the ability to use functions as values is what is called First class Functions (functions can be passed as arguments inside another function, functions can be returned from another function)
 
 
 
